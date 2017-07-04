8d0777d7b262770e11d1eb4c9d2db1c0a90fbc34
ser <sergiydemch@gmail.com>
2017-07-04 16:22:50 +0300

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,314 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <script type="text/javascript" src="/js/main.js"></script>
+        <script type="text/javascript" src="/js/app.js"></script>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="/css/slick-theme.css">
+        <link rel="stylesheet" href="/css/slick.css">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body class="">
+
+        <div class="page-wrap">
+            <div class="itg-header">
+                <div class="itg-top-line-wrap container-fluid">
+                    <div class="container">
+                        <div class="topline-right col-md-6">
+                            <ul class="itg-ul-menu">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Карта сайта</a></li>
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                        </div>
+                        <div class="topline-left cart-line col-md-6 text-right">
+                            <div class="itg-cart-block">
+                                <img src="/img/cart.png" alt="">
+                                <p>Корзина</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="itg-logo-line container-fluid">
+                    <div class="container">
+                        <div class="col-md-3 itg-logo">
+                            <img src="/img/logo.png" alt="" class="site-logo">
+                        </div>
+                        <div class="col-md-3 itg-search">
+                            <form action="handler.php">
+                                <input type=search placeholder="Поиск">
+                                    <input type="submit" class="hidden"><i aria-hidden="true" class="fa fa-search"><span>fa</span></i>
+                            </form>
+                        </div>
+                        <div class="col-md-3 itg-modal">
+                            <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+                        </div>
+                        <div class="col-md-3 itg-phones">
+                            <p><i aria-hidden="true" class="fa fa-phone"><span>fa</span></i><a href="tel:+38 (044)2095051">+38 (044)209-50-51</a></p>
+
+                            <p><i aria-hidden="true" class="fa fa-phone"><span>fa</span></i><a href="tel:+38 (067) 4669608">+38 (067) 46-69-608</a></p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="itg-category-menu container-fluid">
+                    <div class="container">
+                        <ul class="menu nav">
+                            <li class="expanded dropdown">
+                                <a href="" class="dropdown-toggle" data-target="#" data-toggle="dropdown">Гипсовая лепнина <span class="caret"></span></a>
+                                <ul class="menu dropdown-menu">
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/23">Декоративные камины</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/20">Капители</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/7">Карнизы гладкие</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/8">Карнизы с орнаментом</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/18">Колонны</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/17">Консоли и кронштейны</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/13">Купола</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/10">Молдинги</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/11">Молдинги</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/22">Ниши</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/21">Обрамление дверных проемов</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/15">Орнаменты</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/14">Панно</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/19">Пилястры</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/12">Плиты</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/9">Потолочные розетки</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/16">Скульптуры и статуи</a>
+                                    </li>
+                                </ul>
+
+                            </li>
+                            <li>
+                                <a href="" data-drupal-link-system-path="taxonomy/term/29">Декоративные балки</a>
+                            </li>
+                            <li class="expanded dropdown">
+                                <a href="" class="dropdown-toggle" data-target="#" data-toggle="dropdown">Лепнина полиуретановая <span class="caret"></span></a>
+                                <ul class="menu dropdown-menu">
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/26">Карнизы гладкие</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/25">Карнизы с орнаментом</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/27">Молдинги, угловые элементы</a>
+                                    </li>
+                                </ul>
+
+                            </li>
+                            <li>
+                                <a href="" data-drupal-link-system-path="taxonomy/term/28">Полистерол</a>
+                            </li>
+                            <li>
+                                <a href="" data-drupal-link-system-path="taxonomy/term/30">Фасадный декор</a>
+                            </li>
+                            <li class="expanded dropdown">
+                                <a href="" class="dropdown-toggle" data-target="#" data-toggle="dropdown">Ремонт под ключ <span class="caret"></span></a>
+                                <ul class="menu dropdown-menu">
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/32">Котеджи</a>
+                                    </li>
+                                    <li>
+                                        <a href="" data-drupal-link-system-path="taxonomy/term/33">Рестораны</a>
+                                    </li>
+                                </ul>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="itg-slider-wrap container-fluid">
+                <div class="slider-inner slider-for row">
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/1.jpg" alt="1">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/2.jpg" alt="2">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/3.png" alt="3">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/4.jpg" alt="4">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-inner slider-nav orw">
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/1.jpg" alt="1">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/2.jpg" alt="2">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/3.png" alt="3">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="itg-img-slide-item">
+                        <img class="img-fluid"  src="/img/4.jpg" alt="4">
+                        <div class="info">
+                            <h2>slide 1</h2>
+                            <p class="under-slide-title">
+                                some text info
+                            </p>
+                            <p class="description-slide">
+                                some text description on slide
+                            </p>
+                            <a href="">
+                                <button class="btn">кнопка</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="itg-content">
+
+            </div>
+            <div class="itg-footer">
+
+            </div>
+        </div>
+    <div class="itg-modals">
+        <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Modal Header</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Some text in the modal.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
