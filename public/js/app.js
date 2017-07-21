9c0a9e7ebc01cf934b0ed3b65b4a48a597272d8c
ser <sergiydemch@gmail.com>
2017-07-21 11:42:56 +0300

    comm21.07

--- public/js/app.js
+++ public/js/app.js
@@ -77,11 +77,24 @@ module.exports = __webpack_require__(37);
 /***/ (function(module, exports) {
 
 $(document).ready(function () {
+    // $('.dropdown-toggle').dropdown();
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        $('.itg-tabs-slick1').slick("setPosition", 0);
+        $('.itg-tabs-slick2').slick("setPosition", 0);
+        $('.itg-tabs-slick3').slick("setPosition", 0);
+        $('.itg-tabs-slick4').slick("setPosition", 0);
+    });
+
+    // $('.itg-main-tabs .nav-tabs li a').on('click', function () {
+    //     console.log('hhh');
+    // });
 
     $('.slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        nextArrow: '<i class="fa fa-chevron-right"><span>fa</span></i>',
+        prevArrow: '<i class="fa fa-chevron-left"><span>fa</span></i>',
         fade: true,
         asNavFor: '.slider-nav'
     });
@@ -94,6 +107,92 @@ $(document).ready(function () {
         focusOnSelect: true
         // autoplay: true
     });
+
+    /// tabs slider
+
+    $('.itg-tabs-slick1, .itg-tabs-slick2, .itg-tabs-slick3, .itg-tabs-slick4').slick({
+        nextArrow: '<i class="fa fa-chevron-right"><span>fa</span></i>',
+        prevArrow: '<i class="fa fa-chevron-left"><span>fa</span></i>',
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        }]
+    });
+
+    $('.manufactories-inner').slick({
+        nextArrow: '<i class="fa fa-chevron-right"><span>fa</span></i>',
+        prevArrow: '<i class="fa fa-chevron-left"><span>fa</span></i>',
+        infinite: false,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        }]
+    });
+
+    /**  spoiler **/
+
+    // spoiler
+
+    $('.read-more-spoiler').on('click', function () {
+        $('.spoiler-inner').removeClass('spoiler-inner-hidden');
+        $('.read-more-spoiler-span').addClass('display-hidden');
+        $('.read-more-spoiler-close').removeClass('display-hidden');
+    });
+
+    $('.read-more-spoiler-close').on('click', function () {
+        $(this).addClass('display-hidden');
+        $('.spoiler-inner').addClass('spoiler-inner-hidden');
+        $('.read-more-spoiler').removeClass('display-hidden');
+    });
+
+    /**  end spoiler **/
 });
 
 /***/ })
